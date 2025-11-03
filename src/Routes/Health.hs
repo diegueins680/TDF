9db33f5dce12584dea60ac9a6e3724c04a5caa3e
Diego Saa <cuco.saa@gmail.com>
2025-11-02 21:08:13 -0500

    chore: add scripts/smoke.sh to validate /health and demo invoice generation

--- src/Routes/Health.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Routes.Health (HealthAPI, healthServer) where
-
-import Servant
-
- type HealthAPI = "health" :> Get '[PlainText] String
-
- healthServer :: Server HealthAPI
- healthServer = pure "ok"
